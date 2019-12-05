927232f9d78652f3e0d4553dc59643b830e808cc
Aiden <roottor38@gmail.com>
2019-12-05 20:38:23 +0900

    파일 정리

--- feedback/src/main/java/feedback/controller/FeedbackController.java
+++ feedback/src/main/java/feedback/controller/FeedbackController.java
@@ -8,39 +8,22 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import feedback.service.FeedbackService;
 
-@RestController
-=======
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @Controller
->>>>>>> 1527872ed396109e43022de696a1cf557ec392eb
+
 public class FeedbackController {
 	@Autowired
 	FeedbackService service;
 	
-<<<<<<< HEAD
+
 	@GetMapping("/test")
 	public ArrayList<JSONObject> test() throws FileNotFoundException, IOException, ParseException {
 		return service.topWordCount();
-=======
-	@PostMapping("/test")
-//	@GetMapping("/test")
-	public String test() {
-		System.out.println("// accountList");
-		System.out.println("testController");
-		
-		return "test";
->>>>>>> 1527872ed396109e43022de696a1cf557ec392eb
 	}
 	
 	@GetMapping("/key")
